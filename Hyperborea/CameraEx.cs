2ce137819668dac0783a5d2a9902a8b70389d654
Kane Valentine <kane@cute.im>
2024-01-16 22:19:08 +0000

    Zone editor, phase management, Hyperborean Aetherial Compass, updated opcodes for 6.55

---
+++ Hyperborea/CameraEx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hyperborea;
+[StructLayout(LayoutKind.Explicit)]
+public unsafe partial struct CameraEx
+{
+    [FieldOffset(0x60)] public float x;
+    [FieldOffset(0x64)] public float y;
+    [FieldOffset(0x68)] public float z;
+}
---
+++ Hyperborea/ZoneInfo.cs
@@ -0,0 +1,34 @@
+﻿namespace Hyperborea;
+[Serializable]
+public class ZoneInfo
+{
+    [NonSerialized] internal string GUID = Guid.NewGuid().ToString();
+    public string Name = "";
+    public Point3 Spawn;
+    public List<PhaseInfo> Phases = [];
+}
+
+[Serializable]
+public class PhaseInfo
+{
+    [NonSerialized] internal string GUID = Guid.NewGuid().ToString();
+    public string Name = "";
+    public uint Weather = 0;
+    public List<MapEffectInfo> MapEffects = [];
+}
+
+[Serializable]
+public class MapEffectInfo
+{
+    [NonSerialized] internal string GUID = Guid.NewGuid().ToString();
+    public int a1;
+    public int a2;
+    public int a3;
+}
+
+[Serializable]
+public class DirectorInitInfo
+{
+    public uint a1;
+    public uint a2;
+}
